81b5675f117aba78afe27a88e7d290ed5ac2a0a7
so_tea <sohei-a@mbr.nifty.com>
2023-12-26 07:34:33 +0900

    Create abc081a.cpp

---
+++ ABS/abc081a.cpp
@@ -0,0 +1,14 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int ans = 0;
+    for (auto i : s) {
+        if (i == '1')
+            ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
