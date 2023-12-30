2854d8b99de71bae8aa4aea88a28e704c8c32930
so_tea <sohei-a@mbr.nifty.com>
2023-12-30 21:12:30 +0900

    Create 016.cpp

---
+++ 競プロ典型90問/016.cpp
@@ -0,0 +1,18 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n, a, b, c;
+    cin >> n >> a >> b >> c;
+    long long ans = 9999;
+    for (int i = 0; i <= 9999; ++i) {
+        for (int j = 0; j <= 9999; ++j) {
+            long long k = n - a * i - b * j;
+            if (k >= 0 && k%c==0) {
+                ans = min(ans, 1ll * i + j + k / c);
+            }
+        }
+    }
+    cout << ans << endl;
+}
+//つら
